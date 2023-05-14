1b4a48da4761c3ba23216962b2177f5722a31ac1
linhsu0723 <linhsu0723@qq.com>
2023-05-14 11:31:44 +0800

    New: time

---
+++ pylinhsu/time.py
@@ -0,0 +1,19 @@
+import time
+from datetime import datetime
+
+
+EPOCH = datetime(1970, 1, 1)
+
+
+def now():
+    return datetime.now()
+
+
+def sleep(s):
+    time.sleep(s)
+
+
+def delta_hms(ts0, ts1):
+    d = ts1 - ts0
+    epoch_ts = EPOCH + d
+    return epoch_ts.strftime('%H:%M:%S')
---
+++ tests/time.test.py
@@ -0,0 +1,24 @@
+from pylinhsu import time
+from datetime import datetime
+
+
+def test_now():
+    ts = time.now()
+    print(ts)
+
+
+def test_delta_hms():
+    ts0 = time.now()
+    print(ts0)
+
+    time.sleep(2)
+
+    ts1 = time.now()
+    print(ts1)
+
+    print(time.delta_hms(ts0, ts1))
+
+
+if __name__ == '__main__':
+    test_now()
+    test_delta_hms()
